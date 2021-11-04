f6a3d2a328a556550ba550205e291931f77821fd
Ming Xia <ming.xia@wsu.edu>
2021-11-04 14:41:08 -0700

    fix the rex issues

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import searchEngine
+import autoComplete
+import re
 
 app = Flask(__name__)
 
 
+def fliterList(Lists):
+    # tuple list to list
+    lists = [item for items in Lists for item in items]
+    newlist = []
+    for list in lists:
+        list = re.sub(r'\\', '', list)
+        newlist.append(list)
+
+    return newlist
+
+
 @app.route('/', methods=['POST', 'GET'])
-def HomePage():  # put application's code here
-    return render_template('Index.html')
+def homePage():  # put application's code here
+    if request.method == 'POST':
+        query = request.form.get(r'inputWords')
+        if not query:
+            return render_template('index.html')
+        else:
+            rel = searchEngine.querySearch(query)
+            if not rel:
+                return '<h1>No Result<h1>'
+            else:
+                return render_template('Result.html', query=rel)
+    elif request.method == 'GET':
+        auto = autoComplete.autoComplete()
+        auto = fliterList(auto)
+        return render_template('index.html', languages=auto)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
