e6a9902a6f70b88ebb42f7f5087d5a586bdbe9af
Jack Knudson <jknudson729@gmail.com>
2021-11-12 19:29:22 -0800

    Add initial formatting on results

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import searchEngine
 import autoComplete
 import re
+import itertools
 
 app = Flask(__name__)
 
@@ -22,19 +23,28 @@ def homePage():  # put application's code here
     if request.method == 'POST':
         query = request.form.get(r'inputWords')
         if not query:
-            return render_template('index.html')
+            return render_template('Index.html')
         else:
             rel = searchEngine.querySearch(query)
             if not rel:
                 return '<h1>No Result<h1>'
             else:
-                return render_template('Result.html', query=rel)
+                groups = []
+                for i in range(0, len(rel)):
+                    flag = 0
+                    for j in range(0, len(groups)):
+                        if(rel[i][1] in groups[j][0]):
+                            groups[j].append(rel[i])
+                            flag = 1
+                    if(not flag):
+                        groups.append([rel[i]])
+                return render_template('Result.html', query=groups)
     elif request.method == 'GET':
         auto = autoComplete.autoComplete()
         auto = fliterList(auto)
-        return render_template('index.html', languages=auto)
+        return render_template('Index.html', languages=auto)
     else:
-        return render_template('index.html')
+        return render_template('Index.html')
 
 
 if __name__ == '__main__':
