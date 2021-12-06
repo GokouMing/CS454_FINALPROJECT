b0d11ca5549c171d4baf18f3fd1484f7f0f5ce72
Ming Xia <ming.xia@wsu.edu>
2021-12-05 22:09:23 -0800

    New UI and page set up

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import pandas as pd
 from flask import Flask, render_template, request
 import searchEngine
 import autoComplete
 import re
+from flask_paginate import Pagination, get_page_parameter
 
 app = Flask(__name__)
+
 
 def fliterList(Lists):
     # tuple list to list
@@ -12,7 +15,6 @@ def fliterList(Lists):
     for list in lists:
         list = re.sub(r'\\', '', list)
         newlist.append(list)
-
     return newlist
 
 
@@ -25,15 +27,15 @@ def homePage():  # put application's code here
         ad3 = request.form.get(r'grade')
         # This flag uses binary to know which advanced search options were used ex. ad1 + ad2 = 3, ad3 + ad2 = 6
         adFlag = 0
-        if(ad1 != None):
-            if(len(ad1) > 0):
+        if (ad1 != None):
+            if (len(ad1) > 0):
                 adFlag = 1
-        if(ad2 != None):
-            if(len(ad2) > 0):
+        if (ad2 != None):
+            if (len(ad2) > 0):
                 adFlag += 2
-        if(ad3 != None):
-            if(len(ad3) > 0):
-                adFlag += 4              
+        if (ad3 != None):
+            if (len(ad3) > 0):
+                adFlag += 4
         print(ad1, ad2, ad3)
         print(adFlag)
         if not query:
@@ -47,66 +49,77 @@ def homePage():  # put application's code here
                 for i in range(0, len(rel)):
                     flag = 0
                     for j in range(0, len(groups)):
-                        if(rel[i][0] in groups[j][0][0]):
+                        if (rel[i][0] in groups[j][0][0]):
                             groups[j].append(rel[i])
                             flag = 1
                     # Only ran if the item is not in a group already
-                    if(not flag):
-                            if(adFlag == 0):
+                    if (not flag):
+                        if (adFlag == 0):
+                            groups.append([rel[i]])
+                        # Class
+                        elif (adFlag == 1):
+                            # print(ad1)
+                            # print(rel[i])
+                            if (ad1 in rel[i][5]):
                                 groups.append([rel[i]])
-                            # Class
-                            elif(adFlag == 1):
-                                #print(ad1)
-                                #print(rel[i])
-                                if(ad1 in rel[i][5]):
+                        # Quality
+                        elif (adFlag == 2):
+                            # print(ad2)
+                            # print(rel[i])
+                            if (ad2 in rel[i][2]):
+                                groups.append([rel[i]])
+                        # Quality & Class
+                        elif (adFlag == 3):
+                            # print(ad1)
+                            # print(rel[i][2])
+                            if (ad1 in rel[i][5]):
+                                if (ad2 in rel[i][2]):
                                     groups.append([rel[i]])
-                            # Quality
-                            elif(adFlag == 2):
-                                #print(ad2)
-                                #print(rel[i])
-                                if(ad2 in rel[i][2]):
+                        # Grade
+                        elif (adFlag == 4):
+                            # print(ad1)
+                            # print(rel[i])
+                            if (rel[i][3] != None):
+                                if (ad3 in rel[i][3]):
                                     groups.append([rel[i]])
-                            # Quality & Class
-                            elif(adFlag == 3):
-                                #print(ad1)
-                                #print(rel[i][2])
-                                if(ad1 in rel[i][5]):
-                                    if(ad2 in rel[i][2]):
+                        # Class & Grade
+                        elif (adFlag == 5):
+                            # print(ad1)
+                            # print(rel[i])
+                            if (rel[i][3] != None):
+                                if (ad1 in rel[i][5]):
+                                    if (ad3 in rel[i][3]):
                                         groups.append([rel[i]])
-                            # Grade
-                            elif(adFlag == 4):
-                                #print(ad1)
-                                #print(rel[i])
-                                if(rel[i][3] != None):
-                                    if(ad3 in rel[i][3]):
+                        # Quality & Grade
+                        elif (adFlag == 6):
+                            print(ad1)
+                            print(rel[i])
+                            if (rel[i][3] != None):
+                                if (ad2 in rel[i][2]):
+                                    if (ad3 in rel[i][3]):
                                         groups.append([rel[i]])
-                            # Class & Grade
-                            elif(adFlag == 5):
-                                #print(ad1)
-                                #print(rel[i])
-                                if(rel[i][3] != None):
-                                    if(ad1 in rel[i][5]):
-                                        if(ad3 in rel[i][3]):
+                        # Class & Quality & Grade
+                        elif (adFlag == 7):
+                            print(ad1)
+                            print(rel[i])
+                            if (rel[i][3] != None):
+                                if (ad1 in rel[i][5]):
+                                    if (ad2 in rel[i][2]):
+                                        if (ad3 in rel[i][3]):
                                             groups.append([rel[i]])
-                            # Quality & Grade
-                            elif(adFlag == 6):
-                                print(ad1)
-                                print(rel[i])
-                                if(rel[i][3] != None):
-                                    if(ad2 in rel[i][2]):
-                                        if(ad3 in rel[i][3]):
-                                            groups.append([rel[i]])
-                            # Class & Quality & Grade
-                            elif(adFlag == 7):
-                                print(ad1)
-                                print(rel[i])
-                                if(rel[i][3] != None):
-                                    if(ad1 in rel[i][5]):
-                                        if(ad2 in rel[i][2]):
-                                            if(ad3 in rel[i][3]):
-                                                groups.append([rel[i]])
+
+                page = request.args.get(get_page_parameter(), type=int, default=1)
+                pagination = Pagination(page=page,
+                                        total=len(groups),
+                                        css_framework='foundation')
+                print(pagination)
+                print(pagination.page)
+                print(pagination.per_page)
+                print(pagination.args)
+
                 return render_template('Result.html', results=groups, searchquery=query, costTime=round(costtime, 5),
-                                   number=len(groups), similar_query1=query, similar_query2=query)
+                                       number=len(groups), similar_query1=query, similar_query2=query,
+                                       pagination=pagination)
     elif request.method == 'GET':
         auto = autoComplete.autoComplete()
         auto = fliterList(auto)
@@ -115,16 +128,32 @@ def homePage():  # put application's code here
         return render_template('Index.html')
 
 
-@app.route('/result', methods=['POST', 'GET'])
+@app.route('/results', methods=['POST', 'GET'])
 def resultPage():  # put application's code here
     if request.method == 'POST':
-        query = request.form.get(r'inputWords')
+        query = request.form.get(r'Resultsearch')
+        print(query)
         if not query:
             return render_template('index.html')
         else:
             rel, costtime = searchEngine.querySearch(query)
-            return render_template('temresult.html', results=rel, searchquery=query, costTime=round(costtime, 5),
-                                   number=len(rel))
+            if not rel:
+                return '<h1>No Result<h1>'
+            else:
+                groups = []
+                for i in range(0, len(rel)):
+                    flag = 0
+                    for j in range(0, len(groups)):
+                        if (rel[i][0] in groups[j][0][0]):
+                            groups[j].append(rel[i])
+                            flag = 1
+                    # Only ran if the item is not in a group already
+                    if (not flag):
+                        groups.append([rel[i]])
+
+            return render_template('Result.html', results=groups, searchquery=query, costTime=round(costtime, 5),
+                                   number=len(rel), similar_query1=query, similar_query2=query,
+                                   highlightWords=query.split())
     elif request.method == 'GET':
         auto = autoComplete.autoComplete()
         auto = fliterList(auto)
@@ -132,6 +161,9 @@ def resultPage():  # put application's code here
     else:
         return render_template('index.html')
 
+
+# @app.route('/relative', methods=['POST', 'GET'])
+# def relativePage():
 
 if __name__ == '__main__':
     app.run()
