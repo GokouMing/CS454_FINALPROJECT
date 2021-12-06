08385b7c27089aca47c127dfcc2e983e342cdfa2
Ming Xia <ming.xia@wsu.edu>
2021-12-06 06:56:59 -0800

    New UI and page set up

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import pandas as pd
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import searchEngine
 import autoComplete
 import re
-from flask_paginate import Pagination, get_page_parameter
+from flask_paginate import Pagination, get_page_args
 
 app = Flask(__name__)
 
+ad1 = 'None'
+ad2 = 'None'
+ad3 = 'None'
 
 def fliterList(Lists):
     # tuple list to list
@@ -18,108 +20,32 @@ def fliterList(Lists):
     return newlist
 
 
+def newList(list, offset=0, per_page=10):
+    return list[offset: offset + per_page]
+
+
 @app.route('/', methods=['POST', 'GET'])
 def homePage():  # put application's code here
     if request.method == 'POST':
         query = request.form.get(r'inputWords')
         ad1 = request.form.get(r'class')
-        ad2 = request.form.get(r'quality')
-        ad3 = request.form.get(r'grade')
-        # This flag uses binary to know which advanced search options were used ex. ad1 + ad2 = 3, ad3 + ad2 = 6
-        adFlag = 0
-        if (ad1 != None):
-            if (len(ad1) > 0):
-                adFlag = 1
-        if (ad2 != None):
-            if (len(ad2) > 0):
-                adFlag += 2
-        if (ad3 != None):
-            if (len(ad3) > 0):
-                adFlag += 4
-        print(ad1, ad2, ad3)
-        print(adFlag)
-        if not query:
-            return render_template('Index.html')
+        if ad1:
+            query = query + " " + ad1
         else:
-            rel, costtime = searchEngine.querySearch(query)
-            if not rel:
-                return '<h1>No Result<h1>'
-            else:
-                groups = []
-                for i in range(0, len(rel)):
-                    flag = 0
-                    for j in range(0, len(groups)):
-                        if (rel[i][0] in groups[j][0][0]):
-                            groups[j].append(rel[i])
-                            flag = 1
-                    # Only ran if the item is not in a group already
-                    if (not flag):
-                        if (adFlag == 0):
-                            groups.append([rel[i]])
-                        # Class
-                        elif (adFlag == 1):
-                            # print(ad1)
-                            # print(rel[i])
-                            if (ad1 in rel[i][5]):
-                                groups.append([rel[i]])
-                        # Quality
-                        elif (adFlag == 2):
-                            # print(ad2)
-                            # print(rel[i])
-                            if (ad2 in rel[i][2]):
-                                groups.append([rel[i]])
-                        # Quality & Class
-                        elif (adFlag == 3):
-                            # print(ad1)
-                            # print(rel[i][2])
-                            if (ad1 in rel[i][5]):
-                                if (ad2 in rel[i][2]):
-                                    groups.append([rel[i]])
-                        # Grade
-                        elif (adFlag == 4):
-                            # print(ad1)
-                            # print(rel[i])
-                            if (rel[i][3] != None):
-                                if (ad3 in rel[i][3]):
-                                    groups.append([rel[i]])
-                        # Class & Grade
-                        elif (adFlag == 5):
-                            # print(ad1)
-                            # print(rel[i])
-                            if (rel[i][3] != None):
-                                if (ad1 in rel[i][5]):
-                                    if (ad3 in rel[i][3]):
-                                        groups.append([rel[i]])
-                        # Quality & Grade
-                        elif (adFlag == 6):
-                            print(ad1)
-                            print(rel[i])
-                            if (rel[i][3] != None):
-                                if (ad2 in rel[i][2]):
-                                    if (ad3 in rel[i][3]):
-                                        groups.append([rel[i]])
-                        # Class & Quality & Grade
-                        elif (adFlag == 7):
-                            print(ad1)
-                            print(rel[i])
-                            if (rel[i][3] != None):
-                                if (ad1 in rel[i][5]):
-                                    if (ad2 in rel[i][2]):
-                                        if (ad3 in rel[i][3]):
-                                            groups.append([rel[i]])
+            query = query + " " + 'None'
+        ad2 = request.form.get(r'quality')
+        if ad2:
+            query = query + " " + ad2
+        else:
+            query = query + " " + 'None'
+        ad3 = request.form.get(r'grade')
+        if ad3:
+            query = query + " " + ad3
+        else:
+            query = query + " " + 'None'
+        print(query)
+        return redirect(url_for("dataPage", query=query))
 
-                page = request.args.get(get_page_parameter(), type=int, default=1)
-                pagination = Pagination(page=page,
-                                        total=len(groups),
-                                        css_framework='foundation')
-                print(pagination)
-                print(pagination.page)
-                print(pagination.per_page)
-                print(pagination.args)
-
-                return render_template('Result.html', results=groups, searchquery=query, costTime=round(costtime, 5),
-                                       number=len(groups), similar_query1=query, similar_query2=query,
-                                       pagination=pagination)
     elif request.method == 'GET':
         auto = autoComplete.autoComplete()
         auto = fliterList(auto)
@@ -128,42 +54,122 @@ def homePage():  # put application's code here
         return render_template('Index.html')
 
 
-@app.route('/results', methods=['POST', 'GET'])
-def resultPage():  # put application's code here
-    if request.method == 'POST':
-        query = request.form.get(r'Resultsearch')
-        print(query)
-        if not query:
-            return render_template('index.html')
+@app.route('/dataPage/<query>')
+def dataPage(query):
+    list = query.split()
+    print(list)
+    searchE = 5
+    if 'bm25' or 'tfidf' or 'view' in list:
+        query = " ".join(list[:-1])
+        if "bm25" in list:
+            searchE = 0
+        elif "tfidf" in list:
+            searchE = 1
+        else:
+            searchE = 2
+    query = " ".join(list[:-4])
+    print(query, searchE)
+    ad3 = list[-1]
+    ad2 = list[-2]
+    ad1 = list[-3]
+    adFlag = 0
+    if ad1 != 'None':
+        if len(ad1) > 0:
+            adFlag = 1
+    if ad2 != 'None':
+        if len(ad2) > 0:
+            adFlag += 2
+    if ad3 != 'None':
+        if len(ad3) > 0:
+            adFlag += 4
+    print(adFlag)
+    if not query:
+        return render_template('Index.html')
+    else:
+        if searchE == 0:
+            rel, costtime = searchEngine.queryBM25Search(query)
+        elif searchE == 1:
+            rel, costtime = searchEngine.queryWikiIdSearch(query)
+        elif searchE == 2:
+            rel, costtime = searchEngine.querySearch(query)
         else:
             rel, costtime = searchEngine.querySearch(query)
-            if not rel:
-                return '<h1>No Result<h1>'
-            else:
-                groups = []
-                for i in range(0, len(rel)):
-                    flag = 0
-                    for j in range(0, len(groups)):
-                        if (rel[i][0] in groups[j][0][0]):
-                            groups[j].append(rel[i])
-                            flag = 1
-                    # Only ran if the item is not in a group already
-                    if (not flag):
+        if not rel:
+            return '<h1>No Result<h1>'
+        else:
+            groups = []
+            for i in range(0, len(rel)):
+                flag = 0
+                for j in range(0, len(groups)):
+                    if (rel[i][0] in groups[j][0][0]):
+                        groups[j].append(rel[i])
+                        flag = 1
+                # Only ran if the item is not in a group already
+                if not flag:
+                    if adFlag == 0:
                         groups.append([rel[i]])
+                    # Class
+                    elif adFlag == 1:
+                        if ad1 in rel[i][5]:
+                            groups.append([rel[i]])
+                    # Quality
+                    elif adFlag == 2:
+                        if ad2 in rel[i][2]:
+                            groups.append([rel[i]])
+                    # Quality & Class
+                    elif adFlag == 3:
+                        if ad1 in rel[i][5]:
+                            if ad2 in rel[i][2]:
+                                groups.append([rel[i]])
+                    # Grade
+                    elif adFlag == 4:
+                        if rel[i][3] != None:
+                            if ad3 in rel[i][3]:
+                                groups.append([rel[i]])
+                    # Class & Grade
+                    elif adFlag == 5:
+                        if rel[i][3] != None:
+                            if ad1 in rel[i][5]:
+                                if ad3 in rel[i][3]:
+                                    groups.append([rel[i]])
+                    # Quality & Grade
+                    elif adFlag == 6:
+                        if rel[i][3] != None:
+                            if ad2 in rel[i][2]:
+                                if ad3 in rel[i][3]:
+                                    groups.append([rel[i]])
+                    # Class & Quality & Grade
+                    elif adFlag == 7:
+                        if rel[i][3] != None:
+                            if ad1 in rel[i][5]:
+                                if ad2 in rel[i][2]:
+                                    if ad3 in rel[i][3]:
+                                        groups.append([rel[i]])
+
+            page, per_page, offset = get_page_args(page_parameter='page',
+                                                   per_page_parameter='per_page')
+            total = len(groups)
+            groups = newList(groups, offset=offset, per_page=per_page)
+            pagination = Pagination(page=page,
+                                    per_page=per_page,
+                                    offset=offset,
+                                    total=total,
+                                    css_framework='foundation',
+                                    record_name='groups')
 
             return render_template('Result.html', results=groups, searchquery=query, costTime=round(costtime, 5),
-                                   number=len(rel), similar_query1=query, similar_query2=query,
-                                   highlightWords=query.split())
-    elif request.method == 'GET':
-        auto = autoComplete.autoComplete()
-        auto = fliterList(auto)
-        return render_template('index.html', languages=auto)
-    else:
-        return render_template('index.html')
+                                   number=total, similar_query1=query, similar_query2=query,
+                                   pagination=pagination, page=page, per_page=per_page)
 
 
-# @app.route('/relative', methods=['POST', 'GET'])
-# def relativePage():
+@app.route('/results')
+def resultPage():  # put application's code here
+    query = request.args.get('query')
+    sortby = request.args.get('order')
+
+    query = query+" "+sortby
+    return redirect(url_for("dataPage", query=query))
+
 
 if __name__ == '__main__':
     app.run()
--- searchEngine.py
+++ searchEngine.py
@@ -5,7 +5,6 @@ import string
 
 
 def querySearch(query):
-
     s = time.time()
 
     # query filter
@@ -23,7 +22,66 @@ def querySearch(query):
                                         WikiId, OwnerUrl, OwnerSteamId, IconId 
                                 FROM items
                                 WHERE items MATCH " %s " 
-                                ORDER BY bm25(items)
+                                ''' % query)
+    print(query)
+    Database.cursor.execute(command_select_table)
+    result = Database.cursor.fetchall()
+
+    e = time.time()
+    costtime = e - s
+
+    return result, costtime
+
+
+def queryBM25Search(query):
+    s = time.time()
+
+    # query filter
+    Database.dataBaseSetUp()
+    p = re.compile("[" + re.escape(string.punctuation) + "]")
+    query = p.sub(" ", query)
+    # query filter if conatains '%$&\/'
+
+    command_select_table = ('''SELECT   highlight(items,1, '<b>', '</b>')Name, 
+                                        highlight(items,2, '<b>', '</b>')CustomName,
+                                        highlight(items,3, '<b>', '</b>')Quality,
+                                        highlight(items,4, '<b>', '</b>')Description, 
+                                        highlight(items,5, '<b>', '</b>')ItemHeader, 
+                                        highlight(items,6, '<b>', '</b><br>')Class,
+                                        WikiId, OwnerUrl, OwnerSteamId, IconId 
+                                FROM items
+                                WHERE items MATCH " %s " 
+                                ORDER BY bm25(Name)
+                                ''' % query)
+    print(query)
+    Database.cursor.execute(command_select_table)
+    result = Database.cursor.fetchall()
+
+    e = time.time()
+    costtime = e - s
+
+    return result, costtime
+
+
+def queryWikiIdSearch(query):
+    s = time.time()
+
+    # query filter
+    Database.dataBaseSetUp()
+    p = re.compile("[" + re.escape(string.punctuation) + "]")
+    query = p.sub(" ", query)
+    # query filter if conatains '%$&\/'
+
+    command_select_table = ('''SELECT   highlight(items,1, '<b>', '</b>')Name, 
+                                        highlight(items,2, '<b>', '</b>')CustomName,
+                                        highlight(items,3, '<b>', '</b>')Quality,
+                                        highlight(items,4, '<b>', '</b>')Description, 
+                                        highlight(items,5, '<b>', '</b>')ItemHeader, 
+                                        highlight(items,6, '<b>', '</b><br>')Class,
+                                        WikiId, OwnerUrl, OwnerSteamId, IconId 
+                                FROM items
+                                WHERE items MATCH " %s " 
+                                ORDER BY (WikiId)
                                 ''' % query)
     print(query)
     Database.cursor.execute(command_select_table)
