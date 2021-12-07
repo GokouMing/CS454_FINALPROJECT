cba915e662b6dc3f0f7a68bbe7e895b6a466f758
Ming Xia <ming.xia@wsu.edu>
2021-12-07 09:02:00 -0800

    ads update

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import searchEngine
 import autoComplete
 import re
-import string
 from flask_paginate import Pagination, get_page_args
 
 app = Flask(__name__)
